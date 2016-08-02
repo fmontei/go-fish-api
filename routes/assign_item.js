54c8ae8a025bf54545f8cf43e6571bcf67c80ed8
fmontei@g.clemson.edu <fm577c@att.com>
2016-08-02 16:55:57 -0400

    Change assign item to work with item_type/item_name

--- routes/assign_item.js
+++ routes/assign_item.js
@@ -6,19 +6,26 @@ var router = express.Router();
 var db = new sqlite3.Database(process.env.DB_NAME);
 
 router.use(function(req, res, next) {
-    var item_id = req.body.item_id.trim(),
+    var item_id = req.body.item_id,
+    	item_name = req.body.item_name,
+    	item_type = req.body.item_type,
         user_id = req.body.user_id.trim();
         
-    if (!item_id || !user_id) {
-    	res.status(400).send("item_id and user_id must be selected.");
+    if ((!item_id && !user_id) || (!item_name && !item_type && !user_id)) {
+    	res.status(400).send("(item_id and user_id) or (item_name, item_type and user_id) must be provided.");
     }
+
+    var query = "";
+    if (item_id) {
+    	query = "update item set assigned_user_id = " + user_id  + " where item_id = " + item_id + ";";
+    } else {
+    	query = "update item set assigned_user_id = " + user_id  + " where item_name = '" + 
+    		item_name + "' and item_type = '" + item_type + "';";
+    }	
 
     async.waterfall([
         function(callback) {
-            db.run("update item set assigned_user_id = $user_id where item_id = $item_id;", {
-                    $user_id: user_id,
-                    $item_id: item_id
-            }, function(err) {
+            db.run(query, function(err) {
                 callback(err, this.lastID);
             });
         }
